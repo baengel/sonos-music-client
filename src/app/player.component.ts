4b1584d40266a4ad087b77661a86a0d883912aba
Dominic Steger <dominic.steger@baloise.ch>
2025-11-10 12:54:46 +0100

    style

--- src/app/player.component.ts
+++
@@ -1,2 +0,0 @@
-// Diese Datei wurde ins Unterverzeichnis src/app/player/ verschoben und ist hier nicht mehr nötig.
-
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { VolumeControlComponent } from './volume-control.component';
 import {SeekButtonsComponent} from './seek-buttons.component';
-import {StopButtonComponent} from './stop-button.component';
 
 @Component({
   selector: 'app-player',
-  imports: [VolumeControlComponent, SeekButtonsComponent, StopButtonComponent],
+  imports: [VolumeControlComponent, SeekButtonsComponent],
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
