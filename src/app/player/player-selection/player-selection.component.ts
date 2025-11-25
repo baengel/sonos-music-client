a8c7182296493aad42280ecd0d039baa92957b3b
Dominic Steger <dominic.steger@baloise.ch>
2025-11-25 13:13:10 +0100

    player selection

--- src/app/player/player-selection/player-selection.component.ts
+++ src/app/player/player-selection/player-selection.component.ts
@@ -1,13 +1,13 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {BalRadio, BalRadioGroup} from '@baloise/ds-angular';
+import {BalDropdown, BalOption, BalRadio, BalRadioGroup, BalSegment, BalSegmentItem} from '@baloise/ds-angular';
 import {availablePlayers} from '../../app';
 
 @Component({
   selector: 'app-player-selection',
   standalone: true,
-  imports: [CommonModule, FormsModule, BalRadioGroup, BalRadio],
+  imports: [CommonModule, FormsModule, BalRadioGroup, BalRadio, BalSegment, BalSegmentItem, BalDropdown, BalOption],
   templateUrl: './player-selection.component.html',
   styleUrls: ['./player-selection.component.css']
 })
@@ -17,11 +17,23 @@ export class PlayerSelectionComponent {
 
   protected readonly availablePlayers = availablePlayers;
 
+  isMobile: boolean = window.innerWidth < 600;
+
+  constructor() {
+    window.addEventListener('resize', () => {
+      this.isMobile = window.innerWidth < 600;
+    });
+  }
+
   onPlayerSelected(ip: string) {
     this.selectedPlayerIp.emit(ip);
   }
 
   getIp(name: string): string {
     return this.availablePlayers.find(p => p.name === name)?.ip || '';
+  }
+
+  markDocumentAsRead(player: any): void {
+    console.log('PDF für Player gelesen:', player);
   }
 }
